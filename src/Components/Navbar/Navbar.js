27fdb885f70bdee03c764d1830482cfe606914cb
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-08-04 09:23:27 +0600

    update navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import "./Navbar.css";
 import p1 from "../../assets/Profile/p1.jpg";
 import p2 from "../../assets/Profile/p2.jpg";
 import { Link, NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
     const user = true;
@@ -13,42 +14,48 @@ const Navbar = () => {
                 <div className="nav-container">
                     <div className="nav-left">
                         <Link to="/">Logo</Link>
-                    </div>
-                    <div className="nav-center">
-                        <ul className="main-menu">
-                            <li>
-                                <NavLink to="/">Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="about">about</NavLink>
-                            </li>
-                            {user || (
-                                <>
-                                    <li>
-                                        <NavLink to="register">
-                                            Register
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="login">login</NavLink>
-                                    </li>
-                                </>
-                            )}
-                        </ul>
+                        <div id="toggler">
+                            <GiHamburgerMenu />
+                        </div>
                     </div>
                     <div className="nav-right">
-                        {user ? (
-                            <>
-                                <img src={p1} alt="profile" />
-                                <h6>profile name</h6>
-                            </>
-                        ) : (
-                            <>
-                                <NavLink className="nav-btn" to="register">
-                                    Get Start
-                                </NavLink>
-                            </>
-                        )}
+                        <div className="nav-right-left">
+                            <ul className="main-menu">
+                                <li>
+                                    <NavLink to="/">Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="about">about</NavLink>
+                                </li>
+                                {user || (
+                                    <>
+                                        <li>
+                                            <NavLink to="register">
+                                                Register
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="login">login</NavLink>
+                                        </li>
+                                    </>
+                                )}
+                            </ul>
+                        </div>
+                        <div className="nav-right-right">
+                            {user ? (
+                                <>
+                                    <img src={p1} alt="profile" />
+                                    <h6>profile name</h6>
+                                    <button className="nav-btn">logout</button>
+                                </>
+                            ) : (
+                                <>
+                                    <NavLink className="nav-btn" to="register">
+                                        Get Start
+                                    </NavLink>
+                                </>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,11 +1,24 @@
 import React from "react";
+import SingleBlog from "../../Components/SingleBlog/SingleBlog";
 import "./HomePage.css";
 
 const HomePage = () => {
     return (
-        <div>
-            <h1 className="text-primary">this is home page</h1>
-        </div>
+        <section className="sec-wrapper">
+            <div className="sec-container blog-container">
+                <div className="blog-content-part">
+                    <div className="blogs">
+                        <SingleBlog />
+                        <SingleBlog />
+                        <SingleBlog />
+                        <SingleBlog />
+                        <SingleBlog />
+                        <SingleBlog />
+                    </div>
+                </div>
+                <div className="blog-aside-part bg-neutral">aside</div>
+            </div>
+        </section>
     );
 };
 
